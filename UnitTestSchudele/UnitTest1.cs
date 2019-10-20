a26adc05321eca09a4545f918a9a6b9db6cd6a25
lucasaguiarpaixao@gmail.com <lucasaguiarpaixao@gmail.com>
2019-10-20 18:44:58 -0300

    TestUnit

--- UnitTestSchudele/UnitTest1.cs
+++ UnitTestSchudele/UnitTest1.cs
@@ -10,43 +10,49 @@ namespace Schedule.Controllers
     {
         [TestMethod]
         public void CheckDateRange()
-        {
-            DateTime DtStart = DateTime.Now;
-            DateTime DtEnd = DateTime.Now.AddHours(1);
+        {            
+            bool expected = false;
 
-            Schedule schedule = new Schedule();
+            Schedule schedule = new Schedule()
+            {
+                Name = "Pedrinho",
+                DtBirth = Convert.ToDateTime("21/10/1996"),
+                DtStart = Convert.ToDateTime("27/10/2019"),
+                DtExit = Convert.ToDateTime("28/10/2019"),
+                Description = "Note Lucas"
+            };
+
             List<Schedule> scheduleList = new List<Schedule>();
             scheduleList.Add(new Schedule()
             {
                 Name = "Lucas",
                 DtBirth = Convert.ToDateTime("26/12/1991"),
                 DtStart = Convert.ToDateTime("21/10/2019"),
-                DtExit = DtStart.AddHours(1),
+                DtExit = Convert.ToDateTime("22/10/2019"),
                 Description = "Note Lucas"
             });
             scheduleList.Add(new Schedule()
             {
                 Name = "Pedro",
                 DtBirth = Convert.ToDateTime("27/12/1991"),
-                DtStart = Convert.ToDateTime("21/10/2019"),
-                DtExit = DtStart.AddHours(1),
+                DtStart = Convert.ToDateTime("23/10/2019"),
+                DtExit = Convert.ToDateTime("24/10/2019"),
                 Description = "Note Pedro"
             }); scheduleList.Add(new Schedule()
             {
                 Name = "Thiago",
                 DtBirth = Convert.ToDateTime("28/12/1991"),
-                DtStart = Convert.ToDateTime("21/10/2019"),
-                DtExit = DtStart.AddHours(1),
+                DtStart = Convert.ToDateTime("25/10/2019"),
+                DtExit = Convert.ToDateTime("26/10/2019"),
                 Description = "Note Thiago"
             });
 
 
-            ;
 
             schedule.IsValidDate(scheduleList);
 
-            var actual = this;
-            Assert.AreEqual(this, actual, "Calendar cannot be created");
+            bool actual = false;
+            Assert.AreEqual(expected, actual, "Calendar cannot be created");
         }
 
 
